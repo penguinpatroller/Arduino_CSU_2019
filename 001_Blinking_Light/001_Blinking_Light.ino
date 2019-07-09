b7a94518872f20b3f1e7b167f27d4a133178b9a3
camdav <camdav@umich.edu>
2019-07-09 11:32:56 -0500

    Adding arduino file

---
+++ 001_Blinking_Light/001_Blinking_Light.ino
@@ -0,0 +1,16 @@
+/*This code blinks an LED*/
+
+const int led_pin = 7;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(led_pin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(led_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(led_pin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
